577b422a1ed03ca89ef5db256b935ed80457266e
saysamarth <samarth2668@gmail.com>
2025-04-12 22:13:01 +0530

    Add environment config with gitignore protection

--- server/connection.js
+++ server/connection.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
-const password = encodeURIComponent('Samarth@26');
-
-const DB = `mongodb+srv://saysamarth26:${password}@cluster0.vp1c7rv.mongodb.net/skribble?retryWrites=true&w=majority&appName=Cluster0`;
+require('dotenv').config();
 
 async function connectDB() {
-    await mongoose.connect(DB).then(() => {
-        console.log('Connection Succesful!');
-    }).catch((e) => {
-        console.log(e);
-    })
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('MongoDB Connection Successful!');
+    } catch (e) {
+        console.error('Connection Error:', e);
+        process.exit(1);
+    }
 }
 
 module.exports = connectDB;
